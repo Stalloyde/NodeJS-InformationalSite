087db44b7fa08942fc4da39dc9d5ab73244f5324
Stalloyde <stalloyde93@gmail.com>
2023-09-25 16:03:22 +0800

    Fix error handling

--- index.js
+++ index.js
@@ -11,12 +11,22 @@ http
 
     fs.readFile(path, (error, data) => {
       if (error) {
-        res.write(`There's an error`);
+        //handle res error
+        fs.readFile('./error404.html', (error, data) => {
+          if (error) {
+            res.writeHead(400, { 'Content-Type': 'text/plain' });
+            return res.end('Error');
+          }
+          res.writeHead(404, { 'Content-Type': 'text/html' });
+          res.write(data);
+          return res.end();
+        });
+      } else {
+        //handle ok res
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
         return res.end();
       }
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.write(data);
-      return res.end;
     });
   })
   .listen(8080);
